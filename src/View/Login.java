8c2bb2052ba02b0518223a7b051b3deb1b1552db
NguyenBang98 <trongbang09398@gmail.com>
2020-05-05 08:45:36 +0700

    Update

--- src/View/Login.java
+++ src/View/Login.java
@@ -1,65 +1,41 @@
 package View;
 
-import Model.User;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
-public class Login extends JFrame implements ActionListener {
-
-    private User user;
-
+public class Login extends javax.swing.JFrame {
+    
     public Login() {
         super("Login");
         initComponents();
         txtPass.setEchoChar('*');
-        btnLogIn.addActionListener((ActionListener) this);
+        btnLogin.addActionListener((ActionListener) this);
         this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-
+        
     }
-
-    public User getUser() {
-        user = new User(txtMail.getText(), String.valueOf(txtPass.getPassword()));
-        return user;
-    }
-
-    public void showMessage(String msg) {
-        JOptionPane.showMessageDialog(this, msg);
-    }
-
-    public void addLoginListener(ActionListener log) {
-        btnLogIn.addActionListener(log);
-    }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lbUser = new javax.swing.JLabel();
-        lbPass = new javax.swing.JLabel();
-        txtMail = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtUsername = new javax.swing.JTextField();
         txtPass = new javax.swing.JPasswordField();
-        btnLogIn = new javax.swing.JButton();
+        btnLogin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lbUser.setText("User");
+        jLabel1.setText("Tên đăng nhập");
 
-        lbPass.setText("Password");
+        jLabel2.setText("Mật khẩu");
 
-        btnLogIn.setText("LogIn");
-        btnLogIn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnLogInActionPerformed(evt);
-            }
-        });
+        btnLogin.setText("Đăng nhập");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -68,63 +44,52 @@ public class Login extends JFrame implements ActionListener {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(122, 122, 122)
+                        .addGap(142, 142, 142)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lbUser, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lbPass))
-                        .addGap(130, 130, 130)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addGap(83, 83, 83)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtMail, javax.swing.GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
-                            .addComponent(txtPass)))
+                            .addComponent(txtUsername)
+                            .addComponent(txtPass, javax.swing.GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(239, 239, 239)
-                        .addComponent(btnLogIn)))
-                .addContainerGap(126, Short.MAX_VALUE))
+                        .addComponent(btnLogin)))
+                .addContainerGap(183, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(78, 78, 78)
+                .addGap(103, 103, 103)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbUser)
-                    .addComponent(txtMail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(71, 71, 71)
+                    .addComponent(jLabel1)
+                    .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(25, 25, 25)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbPass)
+                    .addComponent(jLabel2)
                     .addComponent(txtPass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(62, 62, 62)
-                .addComponent(btnLogIn)
-                .addContainerGap(93, Short.MAX_VALUE))
+                .addGap(95, 95, 95)
+                .addComponent(btnLogin)
+                .addContainerGap(167, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnLogInActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLogInActionPerformed
-
-    }//GEN-LAST:event_btnLogInActionPerformed
-
     public static void main(String args[]) {
-
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                Login login = new Login();
-                login.setVisible(true);
-                login.setLocationRelativeTo(null);
+                new Login().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnLogIn;
-    private javax.swing.JLabel lbPass;
-    private javax.swing.JLabel lbUser;
-    private javax.swing.JTextField txtMail;
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPasswordField txtPass;
+    private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
--- src/View/TimeTabling.java
+++ src/View/TimeTabling.java
@@ -1,6 +1,7 @@
 package View;
 
 import Controller.TimeTablingDAO;
+import Model.Subject;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -17,7 +18,10 @@ public class TimeTabling extends javax.swing.JFrame implements ActionListener {
             }
         });
         TimeTablingDAO db = new TimeTablingDAO();
-        db.listSubject();
+        for(Subject i : db.listSubject()){
+            cbListSubject.addItem(i.getName().toString());
+        }
+        
     }
 
     public void addSaveListener(ActionListener log) {
